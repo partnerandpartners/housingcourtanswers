4a2b628cca3494f23d0f12420db3a4bed3d35d27
Greg Mihalko <greg@partnerandpartners.com>
2016-11-01 12:44:14 -0400

    Significant work on the For Tenants page, styling the card stack

--- category-for-tenants.php
+++ category-for-tenants.php
@@ -9,11 +9,22 @@
  * @since 		Starkers 4.0
  */
 ?>
-<?php get_header();?>
+<?php get_header();
+// Get the current page category and assign it to global variables
+// $GLOBALS['current_category'] = get_category( get_query_var( 'cat' ) );
+// $GLOBALS['current_category_id'] = $current_category->cat_ID;
+
+$ft_items = wp_get_menu_array('For Tenants');
+	$output_array = array('main' => '', 'scrollspy' => '' );
+// $for_tenants_menu = wp_get_nav_menu_items('For Tenants');
+// print "<pre style='font-size: 10px;'>";
+// print_r($for_tenants_menu);
+// print "</pre>";
+?>
 
 <div class="container-fluid full-bg-green">
 	<div class="container">
-		<?php  get_search_form(); ?>
+		<?php get_search_form(); ?>
 	</div>
 	<div class="container">
 		<div class="row">
@@ -27,312 +38,97 @@
 	</div>
 </div>
 
-<?php $categories = get_terms( 'category', array(
-    'orderby'    => 'term_group',
-    'hide_empty' => 0,
-) );
-foreach ($categories as $category) {
-	echo $category->name;
-}
-?>
+<hr/>
 
 <div class="container">
-   <div class="row">
-	   <div class="col-md-8" role="main">
-
-		  <span class="anchor" id="court"></span>
-		  <div class="first category-page-section">
-				<div class="row">
-					<div class="col-md-12">
-						<?php
-						$the_category = get_category_by_slug('housing-court-tenants');
-						$category_id = $the_category->term_id;
-						$category_link = get_category_link( $category_id );
-						?>
-						<h3 class="section-title"><?php echo get_cat_name($category_id);?></h3>
-						<div class="section-description"><?php echo category_description(6); ?></div>
-					</div>
-
-					<?php
-					$the_category = get_category_by_slug('non-payment-case-tenants');
-					$category_id = $the_category->term_id;
-					$category_link = get_category_link( $category_id );
-					?>
-					<div class="col-md-6 post-category-section">
-						<h4 class="sub-title">
-							<a href="<?php echo esc_url( $category_link ); ?>"><?php echo get_cat_name($category_id);?></a>
-						</h4>
-						<?php echo category_description($category_id); ?>
-					</div>
-
-					<?php
-					$category_id = get_cat_ID( 'How can I get help to pay my back rent?' );
-					$category_link = get_category_link( $category_id );
-					?>
-					<div class="col-md-6 post-category-section">
-						<h5 class="sub-title">
-							<a href="<?php echo esc_url( $category_link ); ?>"><?php echo get_cat_name(8);?></a>
-						</h5>
-						<?php echo category_description(8); ?>
-					</div>
-
-					<?php
-					$category_id = get_cat_ID( 'Holdover Cases' );
-					$category_link = get_category_link( $category_id );
-					?>
-					<div class="col-md-6 post-category-section">
-						<h5 class="sub-title">
-							<a href="<?php echo esc_url( $category_link ); ?>"><?php echo get_cat_name(9);?></a>
-						</h5>
-						<?php echo category_description(9); ?>
-					</div>
-
-					<?php
-					$category_id = get_cat_ID( 'What happens in court?' );
-					$category_link = get_category_link( $category_id );
-					?>
-					<div class="col-md-6 post-category-section">
-						<h5 class="sub-title">
-							<a href="<?php echo esc_url( $category_link ); ?>"><?php echo get_cat_name(12);?></a>
-						</h5>
-						<?php echo category_description(12); ?>
-					</div>
-				</div><!-- .row -->
-		  </div><!-- .first .category-page-section -->
-
-		  <span class="anchor" id="repairs"></span>
-		  <div class="category-page-section highlight">
-		    <?php
-			$args = array('cat'=>'5','-2', 'posts_per_page'=>3, 'order'=>'DESC');
-			$query = new WP_Query($args);
+	<div class="row">
+		<div class="col-md-8">
+			<?php
+			// Loop over each item in the For Tenants Menu
+			foreach ($ft_items as $ft_item ) {
+					$id = (int)$ft_item['ID']; // Get the item ID
+					$title = $ft_item['title']; // Get the item title
+					$type = $ft_item['type'];
+					$link = get_term_link($id); // Get the item link
+					$tip_num = get_category($id)->count; // Get the amount of posts
+					$description = term_description($id); // Get the description
+					$output_array['scrollspy'] .= '<li><a href="#' . $title . '"> ' . $title . '</a></li>';
 			?>
-			<h3 class="section-title"><?php echo get_cat_name(5);?></h3>
-			<h6 class="section-description"><?php echo category_description(5); ?></h6>
-			<hr/>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-				         <article class="post-category-section">
-							<h5 class="small-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<?php the_excerpt(); ?>
-			             </article>
-			    <?php endwhile;?>
+
+			<!-- Top Section of each category -->
+			<a href="<?php echo $link ?>">
+				<h2 id="<?php echo $title ?>"><?php echo $title ?></h2>
+			</a>
+			<p class="lead"><?php echo $description ?></p>
+
+			<?php
+			//echo '<li><a href=" '. $link.' "><h3>' . $title . '</h3></a>' .$description.'</li>';
+			// Check to see if the item has children under it
+			if (!empty($ft_item['children'])) {
+				$ft_item_count = 0; ?>
+
+				<div class="row md-m-b-3">
+
 				<?php
-			    $category_id = get_cat_ID( 'I need repairs in my apartment' );
-				$category_link = get_category_link( $category_id );
-				?>
-			   <a href="<?php echo esc_url( $category_link ); ?>" class="btn btn-lg" role="button">More HP Actions Tips</a>
-		   </div>
+				foreach ($ft_item['children'] as $ft_child) {
+					$sub_id = (int)$ft_child['ID'];
+					$sub_title = $ft_child['title'];
+					$sub_type = $ft_child['type'];
+					if ($sub_type == 'category') {
+						$sub_link = get_term_link($sub_id);
+						$sub_tip_num = get_category($sub_id)->count;
+						$sub_description = term_description($sub_id);
+						$card_class = 'card-stack';
+					} elseif ($sub_type == 'post') {
+						$sub_link = $ft_child['link'];
+							// getting the post excerpt
+							$the_post = get_post($sub_id);
+							setup_postdata($the_post);
+							$sub_description = get_the_excerpt();
+							$card_class = 'card';
+					}
 
-		   <span class="anchor" id="stipulations"></span>
-		   <div class="category-page-section highlight">
-		    <?php
-			$args = array('cat'=>'148','-2', 'posts_per_page'=>2, 'order'=>'ASC');
-			$query = new WP_Query($args);
-			?>
-			<h3 class="section-title"><?php echo get_cat_name(148);?></h3>
-			<h6 class="section-description"><?php echo category_description(148); ?></h6>
-			<hr/>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-				         <article class="post-category-section">
-							<h5 class="small-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<?php the_excerpt(); ?>
-			             </article>
-			    <?php endwhile;?>
-				<?php
-			    $category_id = get_cat_ID( 'Stipulations' );
-				$category_link = get_category_link( $category_id );
-				?>
-			   <a href="<?php echo esc_url( $category_link ); ?>" class="btn btn-lg" role="button">More Stipulation Tips</a>
-		   </div>
+					if ($ft_item_count == 2 ) {  $ft_item_count = 0; ?>
 
-		   <span class="anchor" id="evictions"></span>
-		   <div class="category-page-section highlight">
-		    <?php
-			$args = array('cat'=>'149','-2', 'posts_per_page'=>2, 'order'=>'DESC');
-			$query = new WP_Query($args);
-			?>
-			<h3 class="section-title"><?php echo get_cat_name(149);?></h3>
-			<h6 class="section-description"><?php echo category_description(149); ?></h6>
-			<hr/>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-				         <article class="post-category-section">
-							<h5 class="small-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<?php the_excerpt(); ?>
-			             </article>
-			    <?php endwhile;?>
-				<?php
-			    $category_id = get_cat_ID( 'What will happen during an eviction?' );
-				$category_link = get_category_link( $category_id );
-				?>
-			   <a href="<?php echo esc_url( $category_link ); ?>" class="btn btn-lg" role="button">More Eviction Tips</a>
-		   </div>
-
-		   <span class="anchor" id="order-to-show-cause"></span>
-		   <div class="category-page-section highlight">
-		    <?php
-			$args = array('cat'=>'150','-2', 'posts_per_page'=>2, 'order'=>'DESC');
-			$query = new WP_Query($args);
-			?>
-			<h3 class="section-title"><?php echo get_cat_name(150);?></h3>
-			<h6 class="section-description"><?php echo category_description(150); ?></h6>
-			<hr/>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-				         <article class="post-category-section">
-							<h5 class="small-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<?php the_excerpt(); ?>
-			             </article>
-			    <?php endwhile;?>
-				<?php
-			    $category_id = get_cat_ID( 'How can I stop an eviction?' );
-				$category_link = get_category_link( $category_id );
-				?>
-			   <a href="<?php echo esc_url( $category_link ); ?>" class="btn btn-lg" role="button">More Stop Eviction Tips</a>
-		   </div>
-
-		   <span class="anchor" id="judgements-tenant-screening-and-credit-reports"></span>
-		   <div class="category-page-section highlight">
-		    <?php
-			$args = array('cat'=>'151','-2', 'posts_per_page'=>3, 'order'=>'DESC');
-			$query = new WP_Query($args);
-			?>
-			<h3 class="section-title"><?php echo get_cat_name(151);?></h3>
-			<h6 class="section-description"><?php echo category_description(151); ?></h6>
-			<hr/>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-				         <article class="post-category-section">
-							<h5 class="small-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<?php the_excerpt(); ?>
-			             </article>
-			    <?php endwhile;?>
-				<?php
-			    $category_id = get_cat_ID( 'Judgements, Credit and Tenant Screening Reports' );
-				$category_link = get_category_link( $category_id );
-				?>
-			   <a href="<?php echo esc_url( $category_link ); ?>" class="btn btn-lg" role="button">More Judgement Tips</a>
-		    </div>
-
-
-		    <?php
-			$query = new WP_Query( array( 'post__in' => array( 383 ) ) );
-			?>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-		    <span class="anchor" id="<?php echo( basename(get_permalink()) ); ?>"></span>
-		    <div class="category-page-section highlight">
-			             <h3 class="section-title"><?php the_title(); ?></h3>
-						 <div class="post-content"><?php the_content(); ?></div>
-			    <?php endwhile;?>
-		   <?php wp_reset_query(); ?>
-		   </div>
-
-
-		   <?php
-			$query = new WP_Query( array( 'post__in' => array( 390 ) ) );
-			?>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-		   <span class="anchor" id="<?php echo( basename(get_permalink()) ); ?>"></span>
-		    <div class="category-page-section highlight">
-			             <h3 class="section-title"><?php the_title(); ?></h3>
-						 <div class="post-content"><?php the_content(); ?></div>
-			    <?php endwhile;?>
-		   <?php wp_reset_query(); ?>
-		   </div>
-
-
-		   <span class="anchor" id="roommate-issues"></span>
-		   <div class="category-page-section highlight">
-		    <?php
-			$args = array('cat'=>'155','-2', 'posts_per_page'=>2, 'order'=>'DESC');
-			$query = new WP_Query($args);
-			?>
-			<h3 class="section-title"><?php echo get_cat_name(155);?></h3>
-			<h6 class="section-description"><?php echo category_description(155); ?></h6>
-			<hr/>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-				         <article class="post-category-section">
-							<h5 class="small-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<?php the_content(); ?>
-			             </article>
-			    <?php endwhile;?>
-		    </div>
-
-		   <span class="anchor" id="events-for-tenants"></span>
-		   <div class="category-page-section highlight">
-		    <?php
-			$args = array('cat'=>'2', 'posts_per_page'=>3, 'post_type'=>'event', 'order'=>'DESC');
-			$query = new WP_Query($args);
-			?>
-			<h3 class="section-title">Events for Tenants</h3>
-			<h6 class="section-description">Lorem Ipsum</h6>
-			<hr/>
-				<?php while($query->have_posts()): $query->the_post();?>
-				<?php $postid = get_the_ID(); ?>
-				         <header>
-							<h5 class="small-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<div class="news-event-meta">
-								<span class="small-header">When</span>
-								<div class="event-data">
-									<?php the_field( 'exact_date'); ?>
-								</div>
-								<span class="small-header">Where</span>
-								<div class="event-data">
-									<?php the_field( 'address'); ?>
-								</div>
+					</div><div class="row md-m-b-3">
+						<div class="col-sm-6">
+							<div class="<?php echo $card_class ?>">
+								<a href="<?php echo $sub_link ?>">
+									<h4><?php echo $sub_title ?></h4>
+								</a>
+								<p><?php echo $sub_description ?></p>
 							</div>
-						</header>
-						<div class="xs-top">
-							<?php the_excerpt(); ?>
 						</div>
-			    <?php endwhile;?>
-			   <a href="" class="btn btn-lg" role="button">More Housing Court Events</a>
-		    </div>
 
+					<?php	} else { ?>
+
+					<div class="col-sm-6">
+						<div class="<?php echo $card_class ?>">
+							<a href="<?php echo $sub_link ?>">
+								<h4><?php echo $sub_title ?></h4>
+							</a>
+							<p><?php echo $sub_description ?></p>
+						</div>
+					</div>
+
+					<?php
+					//echo '<li><a href=" '. $sub_link.' ">' . $sub_title . '</a><small>' .$sub_description.'</small></li>';
+					} $ft_item_count++;
+				} ?>
+
+			</div><!-- .row -->
+
+			<?php
+				} else {
+					echo '**Something is wrong here**';
+				}
+			} ?>
+		</div><!-- col-md-8 -->
+
+		<div class="col-md-4 scrollspy">
+			<?php echo $output_array['scrollspy']; ?>
 		</div>
 
-
-		<div id="scroll-spy" class="col-md-3" role="complementary">
-			  <div id="nav">
-			  <ul class="sub-nav nav hidden-xs hidden-sm" data-spy="affix" data-offset-top="432" data-offset-bottom="906">
-			  		<span class="small-header">Tenant Topics</span>
-					<li>
-					  <a href="#court"><?php echo get_cat_name(6);?></a>
-					</li>
-					<li>
-					  <a href="#repairs"><?php echo get_cat_name(5);?></a>
-					</li>
-					<li>
-					  <a href="#stipulations"><?php echo get_cat_name(148);?></a>
-					</li>
-					<li>
-					  <a href="#evictions"><?php echo get_cat_name(149);?></a>
-					</li>
-					<li>
-					  <a href="#order-to-show-cause"><?php echo get_cat_name(150);?></a>
-					</li>
-					<li>
-					  <a href="#judgements-tenant-screening-and-credit-reports"><?php echo get_cat_name(151);?></a>
-					</li>
-					<li>
-					  <a href="#basic-rights-to-know"><?php echo get_the_title('383'); ?></a>
-					</li>
-					<li>
-					  <a href="#apartment-type"><?php echo get_the_title('390'); ?></a>
-					</li>
-					<li>
-					  <a href="#roommate-issues"><?php echo get_cat_name(155);?></a>
-					</li>
-					<li>
-					  <a href="#events-for-tenants">Events for Tenants</a>
-					</li>
-			  </ul>
-			</div>
-		</div>
 	</div>
 </div>
 
--- functions.php
+++ functions.php
@@ -39,9 +39,42 @@ function hca_print_tag_list() {
 	function housingcourtanswers_register_nav_menus() {
 		register_nav_menu( 'main', 'Main Menu' );
 		register_nav_menu( 'footer', 'Footer Menu' );
+		// register_nav_menu( 'fortenants', 'For Tenants Menu' );
 	}
 	add_action( 'after_setup_theme', 'housingcourtanswers_register_nav_menus' );
 
+	/**
+	*
+	* Pulls the menus into a hierarchical array
+	*
+	*/
+	function wp_get_menu_array($current_menu) {
+	    $array_menu = wp_get_nav_menu_items($current_menu);
+	    $menu = array();
+	    foreach ($array_menu as $m) {
+	        if (empty($m->menu_item_parent)) {
+	            $menu[$m->ID] = array();
+	            $menu[$m->ID]['ID']          =   $m->object_id;
+							$menu[$m->ID]['type']        =   $m->object;
+	            $menu[$m->ID]['title']       =   $m->title;
+	            $menu[$m->ID]['url']         =   $m->url;
+
+	            $menu[$m->ID]['children']    =   array();
+	        }
+	    }
+	    $submenu = array();
+	    foreach ($array_menu as $m) {
+	        if ($m->menu_item_parent) {
+	            $submenu[$m->ID] = array();
+	            $submenu[$m->ID]['ID']       =   $m->object_id;
+							$submenu[$m->ID]['type']     =   $m->object;
+	            $submenu[$m->ID]['title']    =   $m->title;
+	            $submenu[$m->ID]['url']      =   $m->url;
+	            $menu[$m->menu_item_parent]['children'][$m->ID] = $submenu[$m->ID];
+	        }
+	    }
+	    return $menu;
+	}
 
 	/**
 	 *
