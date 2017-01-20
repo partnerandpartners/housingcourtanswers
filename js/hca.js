af1e42d6b8718f226449191c5e5586596e336aec
Greg Mihalko <greg@partnerandpartners.com>
2017-01-20 18:37:33 -0500

    Fixing stylistic bugs with the scrollspy, and headers

--- js/hca.js
+++ js/hca.js
@@ -9,23 +9,21 @@ function getOffset(elmnt) {
 
 $( document ).ready(function() {
   var headerHeight = $(".full").outerHeight();
-  if ($('#glossary-labels').length) {
-      var glossaryOffset = $('#glossary-labels').offset().top;
-
-        $('#glossary-labels').affix({
-          offset: (glossaryOffset-60)
-        });
-  }
-
   console.log(headerHeight);
   $( window ).scroll(function() {
-    if (($(window).scrollTop() > headerHeight)) {
+    if (($(window).scrollTop() > 5)) {
       $("header").addClass("bg");
     } else {
       $("header").removeClass("bg");
     }
   });
   //$("li.dropdown:first-child").addClass("dropdown-toggle");
+  if ($('#glossary-labels').length) {
+    var glossaryOffset = $('#glossary-labels').offset().top;
+    $('#glossary-labels').affix({
+      offset: (glossaryOffset-50)
+    });
+  }
 
 
 
