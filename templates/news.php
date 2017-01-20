26831b2e124a82eec0a110538072a6273851a822
Your Name <zach.mihalko@rabweb.com>
2017-01-20 12:52:11 -0500

    changed news thumbnail

--- templates/news.php
+++ templates/news.php
@@ -1,8 +1,9 @@
 <article <?php post_class( 'col-md-4 xs-m-b-3 md-m-b-3' ); ?>>
 	<header class="md-m-b-1">
     <span class="small-header"><?php the_date(); ?></span>
-		<h6 class="news-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+		<h6 class="news-title"><a href="<?php the_field('external_link'); ?>"><?php the_title(); ?></a></h6>
 		<p class="news-subtitle xs-m-b-1"><?php the_field('subtitle'); ?></p>
 	</header>
 	<div class="secondary-type"><?php the_excerpt(); ?></div>
+	<a class="more-link text-uppercase" href="<?php the_field('external_link'); ?>">Read Article</a>
 </article>
